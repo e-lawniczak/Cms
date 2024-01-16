2d266cbf852a5419abf345e04f4a181d102c88a3
Rollo <danielzwierzchowski754@gmail.com>
2024-01-16 22:32:57 +0100

    added serilog to Front

--- backend/PizzeriaAPI/PizzeriaFront/Program.cs
+++ backend/PizzeriaAPI/PizzeriaFront/Program.cs
@@ -1,40 +1,52 @@
-var builder = WebApplication.CreateBuilder(args);
-
-
-// Add services to the container.
-builder.Services.AddRazorPages();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+using Serilog;
+namespace PizzeriaFrontClient
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var builder = WebApplication.CreateBuilder(args);
+
+			builder.Host.UseSerilog((ctx, lc) => lc.ReadFrom.Configuration(ctx.Configuration));
+
+			// Add services to the container.
+			builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    //endpoints.MapRazorPages();
+            //    endpoints.MapControllerRoute(
+            //        name: "default",
+            //        pattern: "{controller=Home}/{action=Index}/{id?}"
+            //        );
+            //});
+
+
+
+            app.Run();
+        }
+    }
 }
-
-
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-//app.UseEndpoints(endpoints =>
-//{
-//    //endpoints.MapRazorPages();
-//    endpoints.MapControllerRoute(
-//        name: "default",
-//        pattern: "{controller=Home}/{action=Index}/{id?}"
-//        );
-//});
-
-
-
-app.Run();
