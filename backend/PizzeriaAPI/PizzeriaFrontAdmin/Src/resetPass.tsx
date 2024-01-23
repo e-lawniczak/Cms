82566e912cca40545bb8c8ca21130e542caf7d0e
e-lawniczak <60924751+e-lawniczak@users.noreply.github.com>
2024-01-23 22:58:47 +0100

    resetpassword feature

--- backend/PizzeriaAPI/PizzeriaFrontAdmin/Src/resetPass.tsx
+++ backend/PizzeriaAPI/PizzeriaFrontAdmin/Src/resetPass.tsx
@@ -2,15 +2,44 @@ import * as React from 'react';
 import { useState } from 'react';
 import * as ReactDOM from 'react-dom';
 
-import { PInput, PageWrapper } from './common';
+import { PInput, PageWrapper, baseApiUrl } from './common';
 import { useForm } from 'react-hook-form';
+import axios from 'axios';
 
-export const PagesPage = () => {
+
+export const ResetPassPage = () => {
     const
         [data, setData] = useState([]),
         { register, handleSubmit } = useForm(),
-        onSubmit = (data:any) =>{
-            if(data.Email !=)
+        params = new URLSearchParams(window.location.search),
+        token = params.get("token"),
+        onSubmit = async (data: any) => {
+            if (data.Email != "" && data.Email.indexOf("@") > -1) {
+                let res = await axios.post(baseApiUrl + "/ForgotPassword", { email: data.Email })
+                console.log(res);
+                if (res.status === 200) {
+                    alert("Email has been sent to " + data.Email)
+                } else {
+                    alert("Something went wrong" + res.statusText)
+                }
+            }
+        },
+        onSubmitReset = async (data: any) => {
+            if (data.password.length > 0 && data.confirmPassword.length > 0) {
+                let res = await axios.post(baseApiUrl + "/Resetpassword", {
+                    resetToken: token,
+                    confirmPassword: data.confirmPassword,
+                    password: data.password
+                })
+                if (res.status == 200) {
+                    alert("Password changed")
+                    window.location.href = "/Login"
+                }
+                else {
+                    alert("Something went wrong. Contact administrator\n" + res.statusText)
+
+                }
+            }
         }
 
 
@@ -26,13 +55,27 @@ export const PagesPage = () => {
             </div>
         </div>
         <div className="row">
-            <form method="post" onSubmit={handleSubmit(onSubmit)}>
-                <div className="form-group">
-                    <PInput register={{ ...register("Email") }} inputProps={{ type:"email", className: 'form-control' }} />
-                </div>
-                <button type="submit">Zresetuj hasło</button>
-                <p> <a href="/Login">Powrót.</a></p>
-            </form>
+            {!!token && token.length > 0 ?
+                <form method="post" onSubmit={handleSubmit(onSubmitReset)} className='account-form login-form'>
+                    <div className="form-group">
+                        <input {...register("password")} placeholder='password' type="password" />
+                    </div>
+                    <div className="form-group">
+                        <input {...register("confirmPassword")} placeholder='confirmPassword' type="password" />
+                    </div>
+                    <button type="submit">Zresetuj hasło</button>
+                    <p> <a href="/Login">Powrót.</a></p>
+                </form>
+                :
+
+                <form method="post" onSubmit={handleSubmit(onSubmit)} className='account-form login-form'>
+                    <div className="form-group">
+                        <input {...register("Email")} type="email" />
+                    </div>
+                    <button type="submit">Zresetuj hasło</button>
+                    <p> <a href="/Login">Powrót.</a></p>
+                </form>
+            }
         </div >
     </>
 
@@ -41,4 +84,4 @@ export const PagesPage = () => {
 
 
 const root = document.getElementById("react_root");
-ReactDOM.render(<PagesPage />, root);
+ReactDOM.render(<ResetPassPage />, root);
